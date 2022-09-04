f966ad9c406fe7d7aaa1a772cc8a94d7157ed244
Fishcount56 <l.fadhilah80@gmail.com>
2022-09-04 12:45:48 +0700

    Update Database

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 6500
+const cors = require('cors')
+const router = require('./src/routes')
+
+app.use(express.json())
+app.use(cors())
+app.use('/api/v1/', router)
+
+app.listen(port, console.log(`[SERVER] server is running on port ${port}`))
---
+++ migrations/20220904054440-create-book.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      booktitle: {
+        type: Sequelize.STRING
+      },
+      bookauthor: {
+        type: Sequelize.STRING
+      },
+      pages: {
+        type: Sequelize.INTEGER
+      },
+      about: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('books');
+  }
+};
